4d89c9e36fd3afba6568ccc74c458ae1a2059d64
paradoxskin <1312269430@qq.com>
2023-01-28 00:47:07 +0800

    adjust space in words and add sup+Shift+o to change wp for work

--- config.h
+++ config.h
@@ -85,53 +85,54 @@ static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY, 			            XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_space,  spawn,          SHCMD("st -c float -A 0.3") },
-	{ 0,                            XK_Print,  spawn,          SHCMD("~/.scripts/screenshot.sh") },
-	{ MODKEY,                       XK_F2,     spawn,          SHCMD("~/.scripts/voltoggle.sh") },
-	{ MODKEY,                       XK_F3,     spawn,          SHCMD("~/.scripts/voldown.sh") },
-	{ MODKEY,                       XK_F4,     spawn,          SHCMD("~/.scripts/volup.sh") },
-	{ MODKEY,                       XK_F5,     spawn,          SHCMD("~/.scripts/bgldown.sh") },
-	{ MODKEY,                       XK_F6,     spawn,          SHCMD("~/.scripts/bglup.sh") },
-	{ MODKEY,                       XK_F1,     spawn,          SHCMD("~/.scripts/switchtouchpad.sh") },
-	{ MODKEY, 			            XK_o,      spawn,          SHCMD("~/.scripts/cgbg.sh") },
+	/* modifier                     key              function        argument */
+	{ MODKEY,                       XK_p,            spawn,          {.v = dmenucmd } },
+	{ MODKEY, 			            XK_Return,       spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_space,        spawn,          SHCMD("st -c float -A 0.3") },
+	{ 0,                            XK_Print,        spawn,          SHCMD("~/.scripts/screenshot.sh") },
+	{ MODKEY,                       XK_F2,           spawn,          SHCMD("~/.scripts/voltoggle.sh") },
+	{ MODKEY,                       XK_F3,           spawn,          SHCMD("~/.scripts/voldown.sh") },
+	{ MODKEY,                       XK_F4,           spawn,          SHCMD("~/.scripts/volup.sh") },
+	{ MODKEY,                       XK_F5,           spawn,          SHCMD("~/.scripts/bgldown.sh") },
+	{ MODKEY,                       XK_F6,           spawn,          SHCMD("~/.scripts/bglup.sh") },
+	{ MODKEY,                       XK_F1,           spawn,          SHCMD("~/.scripts/switchtouchpad.sh") },
+	{ MODKEY, 			            XK_o,            spawn,          SHCMD("~/.scripts/cgbg.sh") },
+	{ MODKEY|ShiftMask,             XK_o,            spawn,          SHCMD("~/.scripts/cgwkbg.sh") },
 	{ MODKEY, 			            XK_backslash,    spawn,          SHCMD("~/.scripts/capture.sh") },
-	{ MODKEY,                       XK_Delete, spawn,          SHCMD("slock") },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_z,      zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY, 			            XK_q,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	//{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	//{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,      0,              0)
-	TAGKEYS(                        XK_2,      1,              0)
-	TAGKEYS(                        XK_3,      2,              0)
-	TAGKEYS(                        XK_4,      3,              0)
-	TAGKEYS(                        XK_5,      4,              0)
-	TAGKEYS(                        XK_6,      5,              0)
-	TAGKEYS(                        XK_7,      6,              0)
-	TAGKEYS(                        XK_8,      7,              0)
-	TAGKEYS(                        XK_9,      8,              0)
-	TAGKEYS(                        XK_f,      2,              "firefox")
-	TAGKEYS(                        XK_m,      3,              "wyy")
-	TAGKEYS(                        XK_v,      8,              "st -e '/home/paradoxd/.scripts/startvpn.sh'")
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY,                       XK_Delete,       spawn,          SHCMD("slock") },
+	{ MODKEY,                       XK_b,            togglebar,      {0} },
+	{ MODKEY,                       XK_j,            focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,            focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_i,            incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,            incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,            setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,            setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_z,            zoom,           {0} },
+	{ MODKEY,                       XK_Tab,          view,           {0} },
+	{ MODKEY, 			            XK_q,            killclient,     {0} },
+	{ MODKEY,                       XK_t,            setlayout,      {.v = &layouts[0]} },
+	//{ MODKEY,                       XK_f,            setlayout,      {.v = &layouts[1]} },
+	//{ MODKEY,                       XK_m,            setlayout,      {.v = &layouts[2]} },
+	{ MODKEY|ShiftMask,             XK_space,        togglefloating, {0} },
+	{ MODKEY,                       XK_0,            view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,            tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,        focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period,       focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,        tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period,       tagmon,         {.i = +1 } },
+	TAGKEYS(                        XK_1,            0,              0)
+	TAGKEYS(                        XK_2,            1,              0)
+	TAGKEYS(                        XK_3,            2,              0)
+	TAGKEYS(                        XK_4,            3,              0)
+	TAGKEYS(                        XK_5,            4,              0)
+	TAGKEYS(                        XK_6,            5,              0)
+	TAGKEYS(                        XK_7,            6,              0)
+	TAGKEYS(                        XK_8,            7,              0)
+	TAGKEYS(                        XK_9,            8,              0)
+	TAGKEYS(                        XK_f,            2,              "firefox")
+	TAGKEYS(                        XK_m,            3,              "wyy")
+	TAGKEYS(                        XK_v,            8,              "st -e '/home/paradoxd/.scripts/startvpn.sh'")
+	{ MODKEY|ShiftMask,             XK_q,            quit,           {0} },
 };
 
 /* button definitions */
