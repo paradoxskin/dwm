b8dc08dc1ac75da953eac617e2a0120c30fcef99
paradoxskin <1312269430@qq.com>
2024-03-28 01:50:40 +0800

    change shortcuts
    add xf86 support

--- config.h
+++ config.h
@@ -95,61 +95,67 @@ static const Key putin_trigger = {MODKEY, XK_equal, switch_putin, {0}};
 static int putin_trigger_keycode;
 static int keycode[300];
 static const Key keys[] = {
-    /* modifier                     key              function        argument */
-    { MODKEY,                       XK_p,            spawn,          {.v = dmenucmd } },
-    { MODKEY,                       XK_Return,       spawn,          {.v = termcmd } },
-    { MODKEY,                       XK_space,        spawn,          SHCMD("st -c float -A 0.5") },
-    { MODKEY,                       XK_Print,        spawn,          SHCMD("~/.scripts/screenshot") },
-    { 0,                            XK_Print,        spawn,          SHCMD("~/.scripts/screenshot area") },
-    { MODKEY,                       XK_F1,           spawn,          SHCMD("~/.scripts/switchtouchpad") },
-    { MODKEY,                       XK_F2,           spawn,          SHCMD("~/.scripts/vol") },
-    { MODKEY,                       XK_F3,           spawn,          SHCMD("~/.scripts/vol down") },
-    { MODKEY,                       XK_F4,           spawn,          SHCMD("~/.scripts/vol up") },
-    { MODKEY,                       XK_F5,           spawn,          SHCMD("~/.scripts/bgl down") },
-    { MODKEY,                       XK_F6,           spawn,          SHCMD("~/.scripts/bgl up") },
-    { MODKEY,                       XK_o,            spawn,          SHCMD("~/.scripts/cgbg") },
-    { MODKEY|ShiftMask,             XK_o,            spawn,          SHCMD("~/.scripts/cgbg pure") },
-    { MODKEY|ShiftMask,             XK_l,            spawn,          SHCMD("~/.scripts/sleep") },
-    { MODKEY|ShiftMask,             XK_Delete,       spawn,          SHCMD("~/.scripts/sleep deep") },
-    { MODKEY,                       XK_Delete,       spawn,          SHCMD("slock") },
-    { MODKEY,                       XK_minus,        spawn,          SHCMD("~/.scripts/orangeScreen") },
-    { MODKEY,                       XK_b,            togglebar,      {0} },
-    { MODKEY,                       XK_j,            focusstack,     {.i = +1 } },
-    { MODKEY,                       XK_k,            focusstack,     {.i = -1 } },
-    { MODKEY,                       XK_i,            incnmaster,     {.i = +1 } },
-    { MODKEY,                       XK_d,            incnmaster,     {.i = -1 } },
-    { MODKEY,                       XK_h,            setmfact,       {.f = -0.05} },
-    { MODKEY,                       XK_l,            setmfact,       {.f = +0.05} },
-    { MODKEY,                       XK_s,            freeze,         {0} },
-    { MODKEY,                       XK_z,            zoom,           {0} },
-    { MODKEY,                       XK_Tab,          lastcli,        {0} },
-    { MODKEY,                       XK_bracketleft,  goprevvis,      {0} },
-    { MODKEY,                       XK_bracketright, gonextvis,      {0} },
-    { MODKEY|ShiftMask,             XK_bracketleft,  goprev,         {0} },
-    { MODKEY|ShiftMask,             XK_bracketright, gonext,         {0} },
-    { MODKEY,                       XK_q,            killclient,     {0} },
-    { MODKEY,                       XK_t,            setlayout,      {.v = &layouts[0]} },
-    { MODKEY|ShiftMask,             XK_space,        togglefloating, {0} },
-    { MODKEY,                       XK_0,            view,           {.ui = ~0 } },
-    { MODKEY|ShiftMask,             XK_0,            tag,            {.ui = ~0 } },
-    { MODKEY,                       XK_comma,        focusmon,       {.i = -1 } },
-    { MODKEY,                       XK_period,       focusmon,       {.i = +1 } },
-    { MODKEY|ShiftMask,             XK_comma,        tagmon,         {.i = -1 } },
-    { MODKEY|ShiftMask,             XK_period,       tagmon,         {.i = +1 } },
-    { MODKEY,                       XK_grave,        debug,          {0} },
-    TAGKEYS(                        XK_1,            0,              0)
-    TAGKEYS(                        XK_2,            1,              0)
-    TAGKEYS(                        XK_3,            2,              0)
-    TAGKEYS(                        XK_4,            3,              0)
-    TAGKEYS(                        XK_5,            4,              0)
-    TAGKEYS(                        XK_6,            5,              0)
-    TAGKEYS(                        XK_7,            6,              0)
-    TAGKEYS(                        XK_8,            7,              0)
-    TAGKEYS(                        XK_9,            8,              0)
-    TAGKEYS(                        XK_f,            2,              "firefox")
-    TAGKEYS(                        XK_m,            3,              "spotify")
-    TAGKEYS(                        XK_v,            8,              "st -e '/home/paradoxd/.scripts/startvpn.sh'")
-    { MODKEY|ShiftMask,             XK_q,            quit,           {0} },
+    /* modifier                     key                         function        argument */
+    { MODKEY,                       XK_p,                       spawn,          {.v = dmenucmd } },
+    { MODKEY,                       XK_Return,                  spawn,          {.v = termcmd } },
+    { MODKEY,                       XK_space,                   spawn,          SHCMD("st -c float -A 0.5") },
+    { MODKEY,                       XK_Print,                   spawn,          SHCMD("~/.scripts/screenshot") },
+    { 0,                            XK_Print,                   spawn,          SHCMD("~/.scripts/screenshot area") },
+    { MODKEY,                       XK_F1,                      spawn,          SHCMD("~/.scripts/switchtouchpad") },
+    { MODKEY,                       XK_F2,                      spawn,          SHCMD("~/.scripts/vol") },
+    { MODKEY,                       XK_F3,                      spawn,          SHCMD("~/.scripts/vol down") },
+    { MODKEY,                       XK_F4,                      spawn,          SHCMD("~/.scripts/vol up") },
+    { MODKEY,                       XK_F5,                      spawn,          SHCMD("~/.scripts/bgl down") },
+    { MODKEY,                       XK_F6,                      spawn,          SHCMD("~/.scripts/bgl up") },
+    { 0,                            XF86XK_AudioMute,           spawn,          SHCMD("~/.scripts/vol") },
+    { 0,                            XF86XK_AudioRaiseVolume,    spawn,          SHCMD("~/.scripts/vol up") },
+    { 0,                            XF86XK_AudioLowerVolume,    spawn,          SHCMD("~/.scripts/vol down") },
+    { 0,                            XF86XK_AudioPlay,           spawn,          SHCMD("~/.scripts/sptfycon") },
+    { 0,                            XF86XK_AudioPrev,           spawn,          SHCMD("~/.scripts/sptfycon prev") },
+    { 0,                            XF86XK_AudioNext,           spawn,          SHCMD("~/.scripts/sptfycon next") },
+    { MODKEY,                       XK_o,                       spawn,          SHCMD("~/.scripts/cgbg") },
+    { MODKEY|ShiftMask,             XK_o,                       spawn,          SHCMD("~/.scripts/cgbg pure") },
+    { MODKEY|ShiftMask,             XK_l,                       spawn,          SHCMD("slock") },
+    { MODKEY,                       XK_Delete,                  spawn,          SHCMD("~/.scripts/sleep") },
+    { MODKEY|ShiftMask,             XK_Delete,                  spawn,          SHCMD("~/.scripts/sleep deep") },
+    { MODKEY,                       XK_minus,                   spawn,          SHCMD("~/.scripts/orangeScreen") },
+    { MODKEY,                       XK_b,                       togglebar,      {0} },
+    { MODKEY,                       XK_j,                       focusstack,     {.i = +1 } },
+    { MODKEY,                       XK_k,                       focusstack,     {.i = -1 } },
+    { MODKEY,                       XK_i,                       incnmaster,     {.i = +1 } },
+    { MODKEY,                       XK_d,                       incnmaster,     {.i = -1 } },
+    { MODKEY,                       XK_h,                       setmfact,       {.f = -0.05} },
+    { MODKEY,                       XK_l,                       setmfact,       {.f = +0.05} },
+    { MODKEY,                       XK_s,                       freeze,         {0} },
+    { MODKEY,                       XK_z,                       zoom,           {0} },
+    { MODKEY,                       XK_Tab,                     lastcli,        {0} },
+    { MODKEY,                       XK_bracketleft,             goprevvis,      {0} },
+    { MODKEY,                       XK_bracketright,            gonextvis,      {0} },
+    { MODKEY|ShiftMask,             XK_bracketleft,             goprev,         {0} },
+    { MODKEY|ShiftMask,             XK_bracketright,            gonext,         {0} },
+    { MODKEY,                       XK_q,                       killclient,     {0} },
+    { MODKEY|ShiftMask,             XK_q,                       quit,           {0} },
+    { MODKEY,                       XK_t,                       setlayout,      {.v = &layouts[0]} },
+    { MODKEY|ShiftMask,             XK_space,                   togglefloating, {0} },
+    { MODKEY,                       XK_0,                       view,           {.ui = ~0 } },
+    { MODKEY|ShiftMask,             XK_0,                       tag,            {.ui = ~0 } },
+    { MODKEY,                       XK_comma,                   focusmon,       {.i = -1 } },
+    { MODKEY,                       XK_period,                  focusmon,       {.i = +1 } },
+    { MODKEY|ShiftMask,             XK_comma,                   tagmon,         {.i = -1 } },
+    { MODKEY|ShiftMask,             XK_period,                  tagmon,         {.i = +1 } },
+    { MODKEY,                       XK_grave,                   debug,          {0} },
+    TAGKEYS(                        XK_1,                       0,              0)
+    TAGKEYS(                        XK_2,                       1,              0)
+    TAGKEYS(                        XK_3,                       2,              0)
+    TAGKEYS(                        XK_4,                       3,              0)
+    TAGKEYS(                        XK_5,                       4,              0)
+    TAGKEYS(                        XK_6,                       5,              0)
+    TAGKEYS(                        XK_7,                       6,              0)
+    TAGKEYS(                        XK_8,                       7,              0)
+    TAGKEYS(                        XK_9,                       8,              0)
+    TAGKEYS(                        XK_f,                       2,              "firefox")
+    TAGKEYS(                        XK_m,                       3,              "spotify")
+    TAGKEYS(                        XK_v,                       8,              "st -e '/home/paradoxd/.scripts/startvpn.sh'")
 };
 
 /* button definitions */
